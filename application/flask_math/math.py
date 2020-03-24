aa460a99ab75a91b47945e46998be57db894d0e2
Terakado Koki <53304697+terakou37910@users.noreply.github.com>
2020-03-24 22:24:03 +0900

    add Taylor series

--- application/flask_math/math.py
+++ application/flask_math/math.py
@@ -1,16 +1,49 @@
 from sympy import *
+from flask import flash
 
 def integral(formula):
     try:
-        anser=integrate(formula)
+        A=str(integrate(formula))
+
+        f=formula.replace("**","B").replace("*","").replace("B","^")
+        A=A.replace("**","B").replace("*","").replace("B","^")
+        anser="∫"+f+"dx = "+A+"+C"
     except:
-        anser="被積分関数を入力してください"
+        anser="Error"
+        flash("もう一度被積分関数を入力してください")
     return anser
 
 
 def derivative(formula):
     try:
-        anser=diff(formula)
+        A=str(diff(formula))
+
+        f=formula.replace("**","B").replace("*","").replace("B","^")
+        A=A.replace("**","B").replace("*","").replace("B","^")
+        anser="d/dx("+f+") = "+A
     except:
-        anser="被積分関数を入力してください"
+        anser="Error"
+        flash("エラー　もう一度関数を入力してください")
+    return anser
+
+
+def taylor(formula,a,b):
+    try:
+        x=symbols("x")
+        a=int(a)
+        b=float(b)
+        f=integrate(formula)
+        f=diff(f)
+
+        A=f.subs(x,b)
+        for number in range(1,int(a)+1,1):
+            f=diff(f)
+            B=f.subs(x,b)/factorial(number)
+            A=B*(x-b)**number+A
+
+        A=str(A)
+        anser=str(formula)+"≒"+str(A.replace("**","B").replace("*","").replace("B","^"))
+    except:
+        anser="Error"
+        flash("エラー　もう一度入力してください")
     return anser
--- application/flask_math/views.py
+++ application/flask_math/views.py
@@ -1,9 +1,9 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_math import app
-from flask_math.math import integral, derivative
+from flask_math.math import integral, derivative, taylor
 
 @app.route("/")
-def show_entries():
+def index_view():
     return render_template("index.html")
 
 @app.route("/integral",methods=["GET","POST"])
@@ -11,7 +11,7 @@ def  integral_view():
     if request.method=="POST":
         formula=request.form.get("formula")
         anser=integral(formula)
-        return render_template("integral.html",anser=anser)
+        return render_template("integral.html",formula=formula,anser=anser)
     else:
         return render_template("integral.html")
 
@@ -21,6 +21,18 @@ def  derivative_view():
     if request.method=="POST":
         formula=request.form.get("formula")
         anser=derivative(formula)
-        return render_template("derivative.html",anser=anser)
+        return render_template("derivative.html",formula=formula,anser=anser)
     else:
         return render_template("derivative.html")
+
+
+@app.route("/taylor",methods=["GET","POST"])
+def  taylor_view():
+    if request.method=="POST":
+        formula=request.form.get("formula")
+        a=request.form.get("a")
+        b=request.form.get("b")
+        anser=taylor(formula,a,b)
+        return render_template("taylor.html",formula=formula,anser=anser,a=a,b=b)
+    else:
+        return render_template("taylor.html")
