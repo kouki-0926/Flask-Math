7c48751abe99b6b1507b9c0bb427192c835e4bc5
Terakado Koki <53304697+terakou37910@users.noreply.github.com>
2020-10-17 11:45:14 +0900

    update templates

--- application/server.py
+++ application/server.py
@@ -1,6 +1,13 @@
-from flask_math import create_app
+import sys
+sys.dont_write_bytecode=True
 
-app=create_app()
+from flask import Flask
+from flask_math.views import Math
+
+app=Flask(__name__)
+app.config.from_object("config")
+app.register_blueprint(Math,url_prefix="/flask_math")
 
 if __name__=="__main__":
     app.run()
+    # app.run(host="0.0.0.0",port=5000)
