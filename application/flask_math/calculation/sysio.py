c68b071a9f94fbd979573126b344df3f77f14d7e
Terakado Koki <terakou37910@icloud.com>
2021-04-24 11:31:53 +0900

    update sysio

--- application/flask_math/calculation/sysio.py
+++ application/flask_math/calculation/sysio.py
@@ -1,34 +1,35 @@
-from flask import make_response
-try:
-    from flask_math.calculation.common.STR import LATEX
-except:
-    from common.STR import LATEX
+from flask_math.calculation.common.STR import LATEX
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 import matplotlib.pyplot as plt
-from control.matlab import *
+from flask import make_response
+from io import BytesIO
 from sympy import *
 import numpy as np
-from io import BytesIO
 
 s, t = symbols('s, t')
-w = symbols('w', real=True)
-
-lower_end_x = -2
-upper_end_x = 5
-num = 300
+num = 150
 
 
-def sysio(formula, formula_2):
-    output = simplify(formula)*simplify(formula_2)
+def sysio(formula, formula_2, lower_end, upper_end):
+    formula = simplify(formula)
+    formula_2 = simplify(formula_2)
+    lower_end = float(lower_end)
+    upper_end = float(upper_end)
+
+    output = apart(formula*formula_2)
     anser = inverse_laplace_transform(output, s, t)
+    FinalValue = limit(anser, t, oo)
+    title = str(factor(anser)).replace("Heaviside(", "u_s(")
 
     # データ作成
-    T = np.linspace(lower_end_x, upper_end_x, num)
+    T = np.linspace(lower_end, upper_end, num)
     Y = np.array([anser.subs(t, T[i]) for i in range(len(T))])
+    F = np.array([FinalValue.subs(t, T[i]) for i in range(len(T))])
 
     fig = plt.figure(figsize=(7, 4))
     plt.plot(T, Y)
-    plt.title("$y(t)="+LATEX(anser)+"("+str(lower_end_x)+"<t<"+str(upper_end_x)+")$")
+    plt.plot(T, F, "--")
+    plt.title("$y(t)="+LATEX(title)+"("+str(lower_end)+"<t<"+str(upper_end)+")$")
 
     # canvasにプロットした画像を出力
     canvas = FigureCanvasAgg(fig)
